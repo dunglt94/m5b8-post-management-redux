e3609616267341b253537f10c13aeae6d3ba860e
dunglt94 <tuandung9594@gmail.com>
2025-02-05 16:35:28 +0700

    test Route 6

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import Post from "./components/Post";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import {Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import NewPost from "./components/NewPost";
 import EditPost from "./components/EditPost";
 
 function App() {
     return (
         <>
-            <Routes>
-                <Route path="/m5b8-post-management-redux"  element={<Post/>}/>
-                <Route path="/m5b8-post-management-redux/add"  element={<NewPost/>}/>
-                <Route path="/m5b8-post-management-redux/edit"  element={<EditPost/>}/>
-            </Routes>
+            <HashRouter basename="m5b8-post-management-redux">
+                <Routes>
+                    <Route path="/" element={<Post/>}/>
+                    <Route path="/add" element={<NewPost/>}/>
+                    <Route path="/edit" element={<EditPost/>}/>
+                </Routes>
+            </HashRouter>
         </>
     );
 }
