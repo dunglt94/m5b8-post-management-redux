84563a70e998f3f1160a6fb6523109bfb3fd6ce4
dunglt94 <tuandung9594@gmail.com>
2025-02-05 16:47:41 +0700

    test Route 8

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import Post from "./components/Post";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import {HashRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NewPost from "./components/NewPost";
 import EditPost from "./components/EditPost";
 
 function App() {
     return (
         <>
-            <HashRouter basename="/m5b8-post-management-redux">
+            <BrowserRouter basename="/m5b8-post-management-redux">
                 <Routes>
                     <Route path="/" element={<Post/>}/>
                     <Route path="/add" element={<NewPost/>}/>
                     <Route path="/edit" element={<EditPost/>}/>
                 </Routes>
-            </HashRouter>
+            </BrowserRouter>
         </>
     );
 }
